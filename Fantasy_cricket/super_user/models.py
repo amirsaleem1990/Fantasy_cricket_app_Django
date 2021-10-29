942c2fa0c752b6192ff845612b3add72e5298a14
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-10-29 14:50:15 +0500

    new

--- Fantasy_cricket/super_user/models.py
+++ Fantasy_cricket/super_user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Players(models.Model):
+	country_id = models.IntegerField()
+	name = models.TextField(max_length=255)
+	category= models.TextField(max_length=255)
+	class Meta:
+		db_table = "Players"
+
+class country(models.Model):
+	name = models.TextField(max_length=255)
+	created_at = models.DateTimeField()
+	class Meta:
+		db_table = "country"
