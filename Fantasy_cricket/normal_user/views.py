ab3c495686bad17a43e42dd6b9a649b5c5da931a
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-02 10:38:27 +0500

    added create_team function

--- Fantasy_cricket/normal_user/views.py
+++ Fantasy_cricket/normal_user/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from super_user.models import *
+from django.db import connection
+cursor = connection.cursor()
 
-# Create your views here.
-# normal_user
 
-def normal_user(request):
-	pass
+
+
+def create_team(request):
+
+	data = cursor.execute('''
+		select c.name, p.name, p.category
+		from players p
+		LEFT JOIN country c
+		ON c.id = p.country_id''').fetchall()
+
+	param = {'bowler' : [],
+            'all_rounder' : [],
+            'wicket_keeper' : [],
+            'batsman' : []}
+	
+	for i in data:
+		param[i[-1]].append((i[1], i[0]))
+
+	return render(request, "create_team.html", param)
