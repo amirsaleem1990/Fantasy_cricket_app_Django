dc9d2ef9465ea8d4801bc6eb5130d7dc324d866e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-02 10:27:38 +0500

    modified user model

--- Fantasy_cricket/Login/models.py
+++ Fantasy_cricket/Login/models.py
@@ -2,15 +2,6 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-# class ExtendedUser(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-#     email      = models.CharField(max_length=100)
-#     first_name = models.CharField(max_length=100)
-#     last_name  = models.CharField(max_length=100)
-#     password   = models.CharField(max_length=100)
-
-
 from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email      = models.CharField(max_length=100)
