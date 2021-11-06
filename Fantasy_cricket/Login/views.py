518a6498098a33f6bf1d77a251bd6b099a8ea1eb
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-06 18:34:53 +0500

    removed authenticate_user function

--- Fantasy_cricket/Login/views.py
+++ Fantasy_cricket/Login/views.py
@@ -3,16 +3,6 @@ from django.http import HttpResponse
 from .models import User
 from django.contrib.auth import authenticate, login
 
-def authenticate_user(email, password):
-	try:
-		user = User.objects.get(email=email)
-	except User.DoesNotExist:
-		return (None, "User not exist.")
-	if user.check_password(password):
-		return (True, user)
-	else:
-		return (None, "Wrong email or password")
-
 def auth(request):
 
 	"""
@@ -25,20 +15,21 @@ def auth(request):
 	# email_address = request.POST['email_address'] 
 	# password      = request.POST['password']
 
-	# user = authenticate_user(email_address, password)
-	# if user[0] is None:
-	# 	error = user[1]
-	# 	return render(request, 'user_not_found.html', {"error" : error})
-	# else:
-	# 	user = user[1]
-	# 	if user.is_active:			
-	# 		login(request, user)
-	# 		if user.is_superuser:
-	# 			return render(request, 'super_user/super_user.html')
-	# 		else:
-	# 			return render(request, 'normal_user/normal_user.html')
+
+	# try:
+	# 	user = User.objects.get(email=email_address)
+	# except User.DoesNotExist:
+	# 	return render(request, 'user_not_found.html', {"error" : "User not exist."})
+	# if not user.check_password(password):
+	# 	return render(request, 'user_not_found.html', {"error" : "Wrong email or password"})
+	# if user.is_active:			
+	# 	login(request, user)
+	# 	if user.is_superuser:
+	# 		return render(request, 'super_user/super_user.html')
 	# 	else:
-	# 		return render(request, 'user_not_found.html', {"error" : "User not found"})
+	# 		return render(request, 'normal_user/normal_user.html')
+	# else:
+	# 	return render(request, 'user_not_found.html', {"error" : "User not active"})
 	return render(request, 'super_user/super_user.html')
 	# NOTE: opar waly code ko uncomment karny k sath hi login.html templace me email and password ko required kar den
 
