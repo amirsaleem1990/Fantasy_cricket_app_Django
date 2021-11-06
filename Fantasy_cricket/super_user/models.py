1ce4d62d809ca7bf8c846752aeef0f4ff12281b9
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-06 18:37:26 +0500

    Added Player_score model | Added 'recorded' field in 'Players' model

--- Fantasy_cricket/super_user/models.py
+++ Fantasy_cricket/super_user/models.py
@@ -7,8 +7,6 @@ class country(models.Model):
 		db_table = "country"
 
 class Players(models.Model):
-	# country_id = models.ForeignKey('country', on_delete=models.CASCADE, db_constraint=False)
-	# country_id = models.ForeignKey(country, on_delete=models.CASCADE)
 	country_id = models.IntegerField()
 	name = models.TextField(max_length=255)
 	category= models.TextField(max_length=255)
@@ -21,5 +19,23 @@ class Matches(models.Model):
 	country_2  = models.TextField(max_length=255)
 	date       = models.DateField()
 	created_at = models.DateTimeField()
+	recorded   = models.IntegerField(default=0)
 	class Meta:
 		db_table = "Matches"
+
+
+class Player_score(models.Model):
+
+	match_id = models.IntegerField()
+	player_id = models.IntegerField()
+	country_id = models.IntegerField()
+
+	player_name = models.TextField(max_length=40)
+	runs = models.IntegerField()
+	wickets = models.IntegerField()
+	catches = models.IntegerField()
+	stumps = models.IntegerField()
+	total = models.IntegerField()
+
+	class Meta:
+		db_table = "Player_score"
