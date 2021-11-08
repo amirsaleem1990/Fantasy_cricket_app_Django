cca2b7cae8c288dcfd99cc7097c42160498fde4a
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-08 18:34:24 +0500

    Initialized leader_board function | Added team_performance

--- Fantasy_cricket/normal_user/views.py
+++ Fantasy_cricket/normal_user/views.py
@@ -8,6 +8,9 @@ from datetime import datetime
 from .models import *
 from Login.models import User
 from django.contrib.auth.decorators import *
+from functools import reduce
+import operator
+from django.db.models import Q
 
 
 def check_normal_user(user):
@@ -101,3 +104,46 @@ def ajax_creation_form(request):
 		                 })
 
 
+@user_passes_test(check_normal_user)
+def team_performance(request):
+	
+	user_id = request.user.id
+	team_created_at = Teams.objects.filter(user_id=user_id).get().created_at
+	matches_to_consider_ids = [i['id'] for i in Matches.objects.filter(created_at__lte=team_created_at, recorded=1).values()]
+
+	players_ids = [i['player_id_in_original_table'] for i in Team_players.objects.filter(user_id=user_id).values()]
+	players = Player_score.objects.filter(reduce(operator.or_, (Q(player_id=x) for x in players_ids))).values()
+	
+	lst = []
+	for i in players:
+		if i['match_id'] in matches_to_consider_ids:
+			lst.append(i)
+	df = pd.DataFrame(lst).groupby('player_id').sum().loc[:, "runs" : "total"].reset_index()
+	df['name'] = df.player_id.apply(lambda x:Players.objects.filter(id=x).get().name)
+	total = df.total.sum()
+	#    player_id  runs  wickets  catches  stumps  total  name
+	# 0        196    20        1        5       0     26   p_2
+	# 1        197     4        9        2       0     15   p_3
+	# 2        199     8        7        8       0     23   p_5
+	# 3        200     7        9        2       0     18   p_6
+	# 4        201     3        4        8       0     15   p_7
+	# 5        207     1        1        3       0      5  p_12
+	# 6        208     1        5        8       0     14  p_13
+	# 7        209    12        0        3       4     19  p_14
+	data = df.apply(pd.Series.to_list, axis=1).to_list()
+	# [[196, 20, 1, 5, 0, 26, 'p_2'],
+	#  [197, 4, 9, 2, 0, 15, 'p_3'],
+	#  [199, 8, 7, 8, 0, 23, 'p_5'],
+	#  [200, 7, 9, 2, 0, 18, 'p_6'],
+	#  [201, 3, 4, 8, 0, 15, 'p_7'],
+	#  [207, 1, 1, 3, 0, 5, 'p_12'],
+	#  [208, 1, 5, 8, 0, 14, 'p_13'],
+	#  [209, 12, 0, 3, 4, 19, 'p_14']]
+	return render(request, "team_performance.html", {"data" : data, 'total' : total})
+
+
+
+
+@user_passes_test(check_normal_user)
+def leader_board(request):
+	return render(request, "leader_board.html")
