0aebe72a7549c7d59e340bf2ce9f8fd4480c6fd9
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-08 14:45:19 +0500

    Since the user name can be duplicated, I droped the 'user' variable in Teams model, and added 'user_id' column to it

--- Fantasy_cricket/normal_user/models.py
+++ Fantasy_cricket/normal_user/models.py
@@ -2,14 +2,13 @@ from django.db import models
 
 class Teams(models.Model):
 
-	user = models.TextField(max_length=20)
+	user_id = models.IntegerField()
 	created_at = models.DateTimeField()
 	class Meta:
 		db_table = "Teams"
 
 class Team_players(models.Model):
-
 	player_id_in_original_table = models.IntegerField()
-	user_id = models.IntegerField
+	user_id = models.IntegerField()
 	class Meta:
 		db_table = "Team_players"
