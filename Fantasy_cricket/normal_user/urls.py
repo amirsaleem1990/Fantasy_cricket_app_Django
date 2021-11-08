055ad6dd920d369c134d332a8ef04ded0ad0d909
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-08 14:48:10 +0500

    styling

--- Fantasy_cricket/normal_user/urls.py
+++ Fantasy_cricket/normal_user/urls.py
@@ -3,8 +3,8 @@ from .views import *
 from django.urls import include, path
 
 urlpatterns = [
-    url("create_team",        create_team,        name="create_team"       ),
-    url("my_form",            my_form,            name="my_form"           ),
+    url("create_team",         create_team,        name="create_team"       ),
+    url("my_form",             my_form,            name="my_form"           ),
     url("ajax_creation_form/", ajax_creation_form, name="ajax_creation_form"),
 ]
 
