329f7ffca3bea46157938b719eb9aca2236c6d82
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-08 14:41:18 +0500

    u

--- Fantasy_cricket/Login/views.py
+++ Fantasy_cricket/Login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse	
 from .models import User
 from django.contrib.auth import authenticate, login
+from normal_user.models import *
 
 def auth(request):
 
@@ -10,27 +11,25 @@ def auth(request):
 	go to  'super_user/super_user.html' otherwise go to 
 	'normal_user/normal_user.html'
 	"""
-	# request.user.username
 	
-	# email_address = request.POST['email_address'] 
-	# password      = request.POST['password']
+	email_address = request.POST['email_address'] 
+	password      = request.POST['password']
 
 
-	# try:
-	# 	user = User.objects.get(email=email_address)
-	# except User.DoesNotExist:
-	# 	return render(request, 'user_not_found.html', {"error" : "User not exist."})
-	# if not user.check_password(password):
-	# 	return render(request, 'user_not_found.html', {"error" : "Wrong email or password"})
-	# if user.is_active:			
-	# 	login(request, user)
-	# 	if user.is_superuser:
-	# 		return render(request, 'super_user/super_user.html')
-	# 	else:
-	# 		return render(request, 'normal_user/normal_user.html')
-	# else:
-	# 	return render(request, 'user_not_found.html', {"error" : "User not active"})
-	return render(request, 'super_user/super_user.html')
-	# NOTE: opar waly code ko uncomment karny k sath hi login.html templace me email and password ko required kar den
+	try:
+		user = User.objects.get(email=email_address)
+	except User.DoesNotExist:
+		return render(request, 'user_not_found.html', {"error" : "User not exist."})
+	if not user.check_password(password):
+		return render(request, 'user_not_found.html', {"error" : "Wrong email or password"})
+	if user.is_active:			
+		login(request, user)
+		if user.is_superuser:
+			return render(request, 'super_user/super_user.html')
+		else:
+			teams_exist = bool(Teams.objects.filter(user_id=request.user.id))
+			return render(request, 'normal_user/normal_user.html', {"teams_exist" : teams_exist})
+	else:
+		return render(request, 'user_not_found.html', {"error" : "User not active"})
 
 
