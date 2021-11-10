81c7420f1910cc9ffca6a6b3d9c947a661a63ed9
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-10 13:33:47 +0500

    u

--- Fantasy_cricket/normal_user/urls.py
+++ Fantasy_cricket/normal_user/urls.py
@@ -3,10 +3,10 @@ from .views import *
 from django.urls import include, path
 
 urlpatterns = [
-    url("create_team",         create_team,        name="create_team"          ),
-    url("my_form",             my_form,            name="my_form"              ),
-    url("ajax_creation_form/", ajax_creation_form, name="ajax_creation_form"   ),
-    url("team_performance",    team_performance,   name="team_performance.html"),
-    url("leader_board",        leader_board,       name="leader_board"         ),
+    url("create_team", create_team, name="create_team"),
+    url("my_form", my_form, name="my_form"),
+    url("ajax_creation_form/", ajax_creation_form, name="ajax_creation_form"),
+    url("team_performance", team_performance, name="team_performance.html"),
+    url("leader_board", leader_board, name="leader_board"),
 ]
 
