fae1064bb2b7b4ef7ebbbf6d4dee68f006343e72
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-10 09:42:38 +0500

    U

--- Fantasy_cricket/Login/views.py
+++ Fantasy_cricket/Login/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from .models import User
 from django.contrib.auth import authenticate, login
 from normal_user.models import *
+from super_user.models import *
+from functools import reduce
+import operator
+from django.db.models import Q
 
 def auth(request):
 
@@ -13,23 +17,27 @@ def auth(request):
 	"""
 	
 	email_address = request.POST['email_address'] 
-	password      = request.POST['password']
+	password = request.POST['password']
 
 
 	try:
 		user = User.objects.get(email=email_address)
 	except User.DoesNotExist:
-		return render(request, 'user_not_found.html', {"error" : "User not exist."})
+		return render(request, 'auth_error.html', {"error" : "User not exist."})
 	if not user.check_password(password):
-		return render(request, 'user_not_found.html', {"error" : "Wrong email or password"})
+		return render(request, 'auth_error.html', {"error" : "Wrong email or password"})
 	if user.is_active:			
 		login(request, user)
 		if user.is_superuser:
 			return render(request, 'super_user/super_user.html')
 		else:
-			teams_exist = bool(Teams.objects.filter(user_id=request.user.id))
-			return render(request, 'normal_user/normal_user.html', {"teams_exist" : teams_exist})
+			my_user_id = request.user.id
+			teams_exist = Teams.objects.filter(user_id=my_user_id)
+			peram = {
+				"teams_exist"    : bool(teams_exist)
+				}
+			return render(request, 'normal_user/normal_user.html', peram)
 	else:
-		return render(request, 'user_not_found.html', {"error" : "User not active"})
-
+		return render(request, 'auth_error.html', {"error" : "User not active"})
+	
 
